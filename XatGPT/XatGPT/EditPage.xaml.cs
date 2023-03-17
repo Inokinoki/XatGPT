3de5f2b402492b65df06b4050aa2f7644a993160
Inokinoki <veyx.shaw@gmail.com>
2023-03-17 10:17:29 +0100

    Add edit page

---
+++ XatGPT/XatGPT/EditPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XatGPT
+{	
+	public partial class EditPage : ContentPage
+	{	
+		public EditPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
