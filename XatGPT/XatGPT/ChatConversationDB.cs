cb1bc2baf1d1814b472736a456ddb8fe12783835
Inokinoki <veyx.shaw@gmail.com>
2023-03-17 13:41:48 +0100

    Add database for ChatConversation objects

---
+++ XatGPT/XatGPT/ChatConversationDB.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace XatGPT
+{
+    /* 
+     * Async lazy load helper from:
+     * https://learn.microsoft.com/zh-cn/xamarin/xamarin-forms/data-cloud/data/databases
+     */
+    public class AsyncLazy<T>
+    {
+        readonly Lazy<Task<T>> instance;
+
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public AsyncLazy(Func<Task<T>> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+    }
+
+    public class ChatConversationDB
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<ChatConversationDB> Instance =
+            new AsyncLazy<ChatConversationDB>(async () =>
+            {
+                var instance = new ChatConversationDB();
+                CreateTableResult result = await Database.CreateTableAsync<ChatConversation>();
+                return instance;
+            });
+
+        public ChatConversationDB()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+        public Task<List<ChatConversation>> GetItemsAsync()
+        {
+            return Database.Table<ChatConversation>().ToListAsync();
+        }
+
+        public Task<ChatConversation> GetItemAsync(int id)
+        {
+            return Database.Table<ChatConversation>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(ChatConversation item)
+        {
+            if (item.Id != 0)
+            {
+                return Database.UpdateAsync(item);
+            }
+            else
+            {
+                return Database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(ChatConversation item)
+        {
+            return Database.DeleteAsync(item);
+        }
+    }
+}
--- XatGPT/XatGPT/MainPage.xaml.cs
+++ XatGPT/XatGPT/MainPage.xaml.cs
@@ -13,9 +13,41 @@ namespace XatGPT
         public MainPage()
         {
             InitializeComponent();
+
+            this.ChatsList.IsRefreshing = true;
+            LoadChatConversations();
+        }
+
+        async void LoadChatConversations()
+        {
+            // Read from sqlite DB
+            var dbInstance = await ChatConversationDB.Instance;
+            var list =
+                await dbInstance.GetItemsAsync();
+            this.ChatsList.ItemsSource = list;
+            if (list.Count > 0) this.ChatsList_Footer.IsVisible = false;
+            else this.ChatsList_Footer.IsVisible = true;
+
+            this.ChatsList.IsRefreshing = false;
         }
 
         void NewChatButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // TODO
+        }
+
+        void ChatsList_Refreshing(System.Object sender, System.EventArgs e)
+        {
+            this.ChatsList.IsRefreshing = true;
+            LoadChatConversations();
+        }
+
+        void DeleteChatItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // TODO
+        }
+
+        void ChatCell_Tapped(System.Object sender, System.EventArgs e)
         {
             // TODO
         }
