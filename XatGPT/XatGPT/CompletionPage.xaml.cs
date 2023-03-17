58a3c1e6f3c8ffdc559544ff6cec61f5261a9690
Inokinoki <veyx.shaw@gmail.com>
2023-03-17 10:15:21 +0100

    Add completion page

---
+++ XatGPT/XatGPT/CompletionPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XatGPT
+{	
+	public partial class CompletionPage : ContentPage
+	{	
+		public CompletionPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
